843345065b3b6cc11a1a81e5615b284f315edbe7
suranjan2003 <karmakarsuranjan12@gmail.com>
2024-02-08 19:13:35 +0530

    comparision test done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+//console.log(2>1)    // checks whether 2 is greater than  1 or not
+//console.log(2!=1)   // checks whether 2 is not equal to 1 or not
+
+//console.log("2" == 2)   //output=> true   but how string is getting compared with an int
+//console.log("2" === 2)  //output => false  bcz now it is checking the datatype too
+
+
+//console.log(false==0)   // checks if false equals to 0  output => true (means yes)
+
