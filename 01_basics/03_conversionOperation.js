a81540d7ad2dbb6654b3f58066498a48b9512ac4
suranjan2003 <karmakarsuranjan12@gmail.com>
2024-02-08 18:55:34 +0530

    done more tricky operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,17 +1,41 @@
 let score = "33"
 
-console.log(typeof score)
+//console.log(typeof score)
 
 let value_in_num = Number(score)  // string to number conversion
 
-console.log(typeof value_in_num)
+//console.log(typeof value_in_num)
 
-// "33" == 33
-// "33abc" == NaN  Not a Number
-// true == 1; false == 0
+// "33" => 33
+// "33abc" => NaN  Not a Number
+// true => 1; false => 0
 
 
 let isLoggedIn = 0
 let boolIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(boolIsLoggedIn) // number to boolean conversion
+
+
+//console.log(boolIsLoggedIn) // number to boolean conversion
+
+// ************************ OPERATIONS *************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// adding two strings
+let str1 = "hello"
+let str2 = " brother"
+
+let strSum = str1 + str2
+console.log(strSum);
+
+// some mislaneous conversions
+/*
+console.log("1" + 2)   => 12
+console.log(1 + "2")   => 12
+console.log("1" + 2 + 2)   => 122
+console.log(1 + 2 + "2")   => 32
+*/
+
