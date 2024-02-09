1e2186e73ebac5e1fe2b563e56b11c1a03f8d830
suranjan2003 <karmakarsuranjan12@gmail.com>
2024-02-09 13:24:37 +0530

    played with dates

--- 01_basics/05_SomeNotes.js
+++ 01_basics/05_SomeNotes.js
@@ -1,18 +1,32 @@
 
-// ************* trimming extra space *************
-
-// const name = "  suranjan karmakar"
+//************* trimming extra space *************
+/*
+const name = "  suranjan karmakar"
 
 //before trim
-// console.log(name)
+console.log(name)
 
 //after trim
-// console.log(name.trim())
-
+console.log(name.trim())
+*/
 
 // ************* changing some elements inside *******
-
+/*
  const url = "https://hitesh.com/hitesh%20choudhury"
 
  console.log(url.replace('%20','-'));  // a copy of url is created and %20 replaced by -
  console.log(url);       // the actual url still contains %20 only
+*/
+
+ //********* Numbers *********************/
+// method 1 of defining number
+const score = 400
+console.log(score)             // observe output
+
+//method 2 of defining numbers
+const balance = new Number(100)
+console.log(balance)            // observe output
+console.log(balance.toString().length)  // converted to string 
+                                        //i.e now we can use all the properties of string in our number too
+console.log(balance.toFixed(2))    // decimal places required
+                                    
---
+++ 01_basics/06_dates.js
@@ -0,0 +1,17 @@
+
+// ********* Dates ***************
+
+/*
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log((myDate.toDateString()))
+*/
+
+// create new date
+let created_date1 = new Date(2013, 3, 23)     // months starts with 0
+console.log((created_date1.toDateString()))
+
+let created_date2 = new Date("2013-3-23")       // months starts with 1
+console.log((created_date2.toDateString()))
+
