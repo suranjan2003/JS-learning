3956ebbc25e81dfa5e4a65b6ab280445fb4db514
suranjan2003 <karmakarsuranjan12@gmail.com>
2024-02-11 01:45:01 +0530

    functions with objects and arrays

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,29 @@
+
+
+// function makeArray(...num1){
+//     return num1
+// }
+// newArray = makeArray(200,400,600)   // array formed
+// console.log(newArray)  
+
+/*
+function makeArray(num1, num2, ...num3){
+    return num3
+}
+newArray = makeArray(200,400,600,700)   // array formed
+console.log(newArray)         // num1 got 200,  num2 got 400,  num3 got rest of the  elements
+*/
+
+//************ arrays and functions************/
+const JsUser = {
+    name: "suranjan",
+    "full name": "suranjan karmakar",
+    age: 18,
+    location: "Jaipur"
+}
+
+function handleObj(anyObj){
+    console.log(anyObj.location)
+}
+
+handleObj(JsUser)
